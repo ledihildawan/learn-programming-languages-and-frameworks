86ec82b10212918f9199a31c1c51d8c0b2bf7aca
ledihildawan <lhildawan@gmail.com>
2019-06-13 22:24:38 +0800

    add traza

---
+++ traza/js/main.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const mainHeader = document.querySelector(".main-header");
+  const menuButton = document.querySelector(".menu-button");
+  const mainNav = document.querySelector(".main-nav");
+  const siteListLinks = document.querySelectorAll(".site-list__link");
+  const siteListLinksArr = Array.from(siteListLinks);
+
+  menuButton.addEventListener("click", function() {
+    mainNav.classList.toggle("open");
+    this.classList.toggle("active");
+  });
+
+  siteListLinksArr.forEach(siteListLink => {
+    siteListLink.addEventListener("click", function() {
+      mainNav.classList.toggle("open");
+    });
+  });
+
+  window.addEventListener("scroll", function() {
+    if (this.scrollY > 1) {
+      mainHeader.classList.add("sticky");
+    } else {
+      mainHeader.classList.remove("sticky");
+    }
+  });
+});
