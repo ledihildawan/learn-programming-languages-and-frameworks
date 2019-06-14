01f3b5798fd092e3531c7752da272e782ad9c031
ledihildawan <lhildawan@gmail.com>
2019-06-14 15:18:02 +0800

    traza final

--- traza/js/main.js
+++ traza/js/main.js
@@ -4,23 +4,54 @@ window.addEventListener("DOMContentLoaded", () => {
   const mainNav = document.querySelector(".main-nav");
   const siteListLinks = document.querySelectorAll(".site-list__link");
   const siteListLinksArr = Array.from(siteListLinks);
+  const goToUp = document.querySelector(".gotoup");
+  const hero = document.querySelector(".hero");
 
   menuButton.addEventListener("click", function() {
     mainNav.classList.toggle("open");
     this.classList.toggle("active");
   });
 
-  // siteListLinksArr.forEach(siteListLink => {
-  //   siteListLink.addEventListener("click", function() {
-  //     mainNav.classList.toggle("open");
-  //   });
-  // });
+  if (window.innerWidth < 1024) {
+    console.log(window.innerWidth);
+    siteListLinksArr.forEach(siteListLink => {
+      siteListLink.addEventListener("click", function() {
+        mainNav.classList.toggle("open");
+      });
+    });
+  } else {
+    const wow = new WOW({
+      boxClass: "wow",
+      animateClass: "animated",
+      offset: 0,
+      mobile: false,
+      live: true
+    });
+
+    wow.init();
+  }
 
   window.addEventListener("scroll", function() {
     if (this.scrollY > 1) {
       mainHeader.classList.add("sticky");
     } else {
       mainHeader.classList.remove("sticky");
+    }
+
+    if (window.innerWidth < 1024) {
+      if (this.scrollY > 560) {
+        goToUp.style.opacity = 1;
+        goToUp.style.visibility = "visible";
+      } else {
+        goToUp.style.opacity = 0;
+      }
+    } else {
+      if (this.scrollY > hero.clientHeight - 400) {
+        goToUp.style.opacity = 1;
+        goToUp.style.visibility = "visible";
+      } else {
+        goToUp.style.opacity = 0;
+      }
     }
   });
 });
