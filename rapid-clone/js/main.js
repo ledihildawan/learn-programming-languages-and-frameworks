f402f1b89dee8f37211246d9817305c298535b5f
ledihildawan <lhildawan@gmail.com>
2019-06-20 01:20:28 +0800

    add feature client list to rapid-clone

--- rapid-clone/js/main.js
+++ rapid-clone/js/main.js
@@ -1,107 +1,109 @@
-$(document).ready(function() {
-  if (window.innerWidth > 1024) {
-    const wow = new WOW({
-      boxClass: "wow",
-      animateClass: "animated",
-      offset: 0,
-      mobile: false,
-      live: true
-    });
-    wow.init();
-  }
+$(document).ready(function () {
+  // if (window.innerWidth > 1024) {
+  //   const wow = new WOW({
+  //     boxClass: "wow",
+  //     animateClass: "animated",
+  //     offset: 0,
+  //     mobile: false,
+  //     live: true
+  //   });
+  //   wow.init();
+  // }
 
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    loop: true
+  $(".client__list").owlCarousel({
+    items: 2,
+    loop: true,
+    autoplay: true,
+    margin: 24
   });
 
-  // Vanilla
-  const newsletterForm = document.forms["newsletter"];
+  // // Vanilla
+  // const newsletterForm = document.forms["newsletter"];
 
-  function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
+  // function validateEmail(email) {
+  //   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //   return re.test(email);
+  // }
 
-  function validate() {
-    var email = $("#email").val();
+  // function validate() {
+  //   var email = $("#email").val();
 
-    if (validateEmail(email)) {
-      $("#email").val("");
-      $("form[name='newsletter']").fadeOut(1000);
+  //   if (validateEmail(email)) {
+  //     $("#email").val("");
+  //     $("form[name='newsletter']").fadeOut(1000);
 
-      setTimeout(() => {
-        $(".newsletter .container")
-          .append("<h3>Now you are on the list! Thank for have been joined.</h3>");
+  //     setTimeout(() => {
+  //       $(".newsletter .container")
+  //         .append("<h3>Now you are on the list! Thank for have been joined.</h3>");
 
-        $(".newsletter h3")
-          .css({
-            "color": "#64c29e",
-            "text-align": "center",
-            "line-height": "1.5"
-          })
-          .hide()
-          .fadeIn(1000);
-      }, 1000);
+  //       $(".newsletter h3")
+  //         .css({
+  //           "color": "#64c29e",
+  //           "text-align": "center",
+  //           "line-height": "1.5"
+  //         })
+  //         .hide()
+  //         .fadeIn(1000);
+  //     }, 1000);
 
-      setTimeout(() => {
-        $(".newsletter h3").fadeOut(1000);
-      }, 4000);
+  //     setTimeout(() => {
+  //       $(".newsletter h3").fadeOut(1000);
+  //     }, 4000);
 
-      setTimeout(() => {
-        $(".newsletter h3").remove();
-      }, 4999);
+  //     setTimeout(() => {
+  //       $(".newsletter h3").remove();
+  //     }, 4999);
 
-      setTimeout(() => {
-        $("#email").css("border-color", "#d3d7d9");
-        $("form button").text("Go");
-        $("form[name='newsletter']").fadeIn(1000);
-      }, 5000);
-    } else {
-      $("#email").css("border-color", "#FFBABA");
-      $("form[name='newsletter']").addClass("shake");
+  //     setTimeout(() => {
+  //       $("#email").css("border-color", "#d3d7d9");
+  //       $("form button").text("Go");
+  //       $("form[name='newsletter']").fadeIn(1000);
+  //     }, 5000);
+  //   } else {
+  //     $("#email").css("border-color", "#FFBABA");
+  //     $("form[name='newsletter']").addClass("shake");
 
-      setTimeout(() => {
-        $("#email").css("border-color", "#d3d7d9");
-      }, 2000);
-    }
+  //     setTimeout(() => {
+  //       $("#email").css("border-color", "#d3d7d9");
+  //     }, 2000);
+  //   }
 
-    return false;
-  }
+  //   return false;
+  // }
 
-  newsletterForm.addEventListener("submit", function(e) {
-    e.preventDefault();
-    validate();
-  });
+  // newsletterForm.addEventListener("submit", function(e) {
+  //   e.preventDefault();
+  //   validate();
+  // });
 
-  // Sticky Main Navbar
-  window.addEventListener("scroll", function() {
-    if (window.scrollY > 1) {
-      document.querySelector(".main-header").classList.add("sticky");
-    } else {
-      document.querySelector(".main-header").classList.remove("sticky");
-    }
-  });
+  // // Sticky Main Navbar
+  // window.addEventListener("scroll", function() {
+  //   if (window.scrollY > 1) {
+  //     document.querySelector(".main-header").classList.add("sticky");
+  //   } else {
+  //     document.querySelector(".main-header").classList.remove("sticky");
+  //   }
+  // });
 
-  // Show Mobile Navbar
-  const mobileNavBar = document.querySelector(".main-nav-mobile");
-  const menuBtn = document.querySelector(".menu-btn");
-  const closeMobileNav = document.querySelector(".main-nav-mobile__close");
+  // // Show Mobile Navbar
+  // const mobileNavBar = document.querySelector(".main-nav-mobile");
+  // const menuBtn = document.querySelector(".menu-btn");
+  // const closeMobileNav = document.querySelector(".main-nav-mobile__close");
 
-  menuBtn.addEventListener("click", function() {
-    if (mobileNavBar.classList.contains("close")) {
-      mobileNavBar.classList.remove("close");
-      mobileNavBar.classList.add("open");
-    } else {
-      mobileNavBar.classList.remove("open");
-      mobileNavBar.classList.add("close");
-    }
-  });
+  // menuBtn.addEventListener("click", function() {
+  //   if (mobileNavBar.classList.contains("close")) {
+  //     mobileNavBar.classList.remove("close");
+  //     mobileNavBar.classList.add("open");
+  //   } else {
+  //     mobileNavBar.classList.remove("open");
+  //     mobileNavBar.classList.add("close");
+  //   }
+  // });
 
-  closeMobileNav.addEventListener("click", function() {
-    if (mobileNavBar.classList.contains("open")) {
-      mobileNavBar.classList.add("close");
-      mobileNavBar.classList.remove("open");
-    }
-  });
+  // closeMobileNav.addEventListener("click", function() {
+  //   if (mobileNavBar.classList.contains("open")) {
+  //     mobileNavBar.classList.add("close");
+  //     mobileNavBar.classList.remove("open");
+  //   }
+  // });
 });
