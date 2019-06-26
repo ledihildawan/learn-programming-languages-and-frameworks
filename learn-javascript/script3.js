bd0f6eb78220e9bcb64da1ea3d70a87bff357f4a
ledihildawan <lhildawan@gmail.com>
2019-06-27 00:59:03 +0800

    learn DOM

---
+++ learn-javascript/script3.js
@@ -0,0 +1,20 @@
+// DOM Selectors
+document.getElementsByTagName();
+document.getElementsByClassName();
+document.getElementById();
+
+document.querySelector();
+document.querySelectorAll();
+
+getAttribute();
+setAttriute();
+
+// Changing Styles
+style.{propery}
+
+className
+classList
+
+classList.add()
+classList.remove()
+classList.toggle()
