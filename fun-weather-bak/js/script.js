1c3c6e6176f181f2d517d6f1f67815db0a7c85ea
ledihildawan <lhildawan@gmail.com>
2019-07-14 05:54:32 +0800

    exe leasson 25

--- fun-weather-bak/js/script.js
+++ fun-weather-bak/js/script.js
@@ -6,4 +6,48 @@ $(document).ready(function () {
   $('.works__btns button, .works__btns a').on('blur', function (e) {
     $(this).parents('.works__element').removeClass('works__element--active');
   });
+
+  function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+    const progressElement = $(node);
+    progressElement.each(function (value, item) {
+      $(item).find(nodeLine).animate({
+        width: item.dataset.progressPercent+'%',
+      }, animationLength);
+      $(item).find(tooltip).show(animationLength);
+    });
+  }
+
+  let animate = true;
+
+  $(window).scroll(function () {
+    if ($('.skills').offset().top <= $(window).scrollTop() + 150) {
+      if (animate) {
+        moveProgressBar('.progress__element', '.progress__line', 'progress__tooltip');
+      }
+      animate = false;
+    }
+  });
+
+  $('.carousel').owlCarousel({
+    loop: true,
+    margin: 25,
+    nav: true,
+    dots: false,
+    navText: [],
+    items: 5,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      980: {
+        items: 5
+      }
+    }
+  })
 });
