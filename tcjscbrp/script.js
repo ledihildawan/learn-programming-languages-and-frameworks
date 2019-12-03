0a6de018ef9cfb0e36c7a654657787d08a2e7e4d
Ledi Hildawan <lhildawan@gmail.com>
2019-12-04 02:12:49 +0800

    coding challenge 2

--- tcjscbrp/script.js
+++ tcjscbrp/script.js
@@ -191,7 +191,7 @@ if (markBMI > johnBMI) {
 /****************************************
  * Boolean logic
  */
-
+/*
 var firstName = "John";
 var age = 20;
 
@@ -203,4 +203,125 @@ if (age < 13) {
   console.log(firstName + " is a young man.");
 } else {
   console.log(firstName + " is a man.");
+}
+*/
+
+/****************************************
+ * The Ternary Operator and Switch Statements
+ */
+/*
+// Ternary operator
+var firstName = "John";
+var age = 22;
+
+age >= 18
+  ? console.log(firstName + " drinks beer.")
+  : console.log(firstName + " drinks juice");
+
+var drink = age >= 18 ? "beer" : "juice";
+console.log(drink);
+
+/* if (age >= 18) {
+  var drink = "beer";
+} else {
+  var drink = "juice";
+} */
+
+/*
+// Switch statement
+var job = "instructor";
+switch (job) {
+  case "teacher":
+  case "instructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + " drives an uber in Lisbon.");
+    break;
+  case "designer":
+    console.log(firstName + " designs beutiful websites.");
+    break;
+  default:
+    console.log(firstName + " does something else.");
+    break;
+}
+
+age = 56;
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy.");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teenager.");
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + " is a young man.");
+    break;
+  default:
+    console.log(firstName + " is a man");
+}
+*/
+
+/****************************************
+ * Truthy and Falsy values and equality operators
+ */
+/*
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 0;
+
+if (height || height === 0) {
+  console.log("Variable is defined");
+} else {
+  console.log("Variable ha not been defined");
+}
+
+// Equality operators
+if (height === "0") {
+  console.log("The == operator does type coercion!");
+}
+*/
+
+/****************************************
+ * Codding Challenge 2
+ */
+
+var johnsTeamScore, mikesTeamScore, marysTeamScore;
+
+johnsTeamScore = 89;
+johnsTeamScore += 120;
+johnsTeamScore += 103;
+
+mikesTeamScore = 116;
+mikesTeamScore += 94;
+mikesTeamScore += 123;
+
+marysTeamScore = 97;
+marysTeamScore += 134;
+marysTeamScore += 105;
+
+var johnsTeamScoreAvg = johnsTeamScore / 3;
+var mikesTeamScoreAvg = mikesTeamScore / 3;
+var marysTeamScoreAvg = marysTeamScore / 3;
+
+if (
+  johnsTeamScoreAvg > mikesTeamScoreAvg &&
+  johnsTeamScoreAvg > marysTeamScoreAvg
+) {
+  console.log("John's team is the winner!");
+} else if (
+  mikesTeamScoreAvg > johnsTeamScoreAvg &&
+  mikesTeamScoreAvg > marysTeamScoreAvg
+) {
+  console.log("Mike's team is the winner!");
+} else if (
+  marysTeamScoreAvg > johnsTeamScoreAvg &&
+  marysTeamScoreAvg > mikesTeamScoreAvg
+) {
+  console.log("Mary's team is the winner!");
+} else {
+  console.log("Draw. That's the bored game i ever see");
 }
