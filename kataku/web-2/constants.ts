f7beaf5b3f0803b30caf63911b8bea41c5c5c871
ledihildawan <ledi.hildawan@salt.co.id>
2025-04-11 20:34:59 +0700

    chore: remove the web-2

--- kataku/web-2/constants.ts
+++
@@ -1,5 +0,0 @@
-export const DB_URL = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-export const URL_API =
-  typeof window === 'undefined' ? `http://localhost:${process.env.PORT ?? 3000}` : window.location.origin;
-export const SERVER_PORT = process.env.SERVER_PORT || 44720;
-export const APP_PORT = process.env.APP_PORT || 32462;
--- kataku/web-2/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['http://localhost:3000', 'http://localhost:32462', 'http://localhost:44720'],
-};
-
-export default nextConfig;
