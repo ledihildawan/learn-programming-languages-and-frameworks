d764a533630f156ddb6cefc4c8945f1a709157aa
ledihildawan <ledi.hildawan@salt.co.id>
2025-04-12 23:51:39 +0700

    learn: js abort

---
+++ javascript/abort.js
@@ -0,0 +1,23 @@
+const controller = new AbortController();
+const signal = controller.signal;
+
+const url = "video.mp4";
+const downloadBtn = document.querySelector(".download");
+const abortBtn = document.querySelector(".abort");
+
+downloadBtn.addEventListener("click", fetchVideo);
+
+abortBtn.addEventListener("click", () => {
+  controller.abort();
+  console.log("Download aborted");
+});
+
+function fetchVideo() {
+  fetch(url, { signal })
+    .then((response) => {
+      console.log("Download complete", response);
+    })
+    .catch((err) => {
+      console.error(`Download error: ${err.message}`);
+    });
+}
