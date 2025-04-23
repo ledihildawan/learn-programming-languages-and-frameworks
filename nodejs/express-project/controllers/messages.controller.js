54f816790f16e102aa96c7aeb9cda4cd8fe3feb3
Ledi Hildawan <lhildawan@gmail.com>
2025-04-24 01:26:34 +0700

    Layouts_and_Separation_of_Concerns-

--- nodejs/express-project/controllers/messages.controller.js
+++ nodejs/express-project/controllers/messages.controller.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
+  // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
+  res.render('messages', { friend: 'James' });
 }
 
 function postMessage(req, res) {
--- nodejs/express-project/routes/messages.router.js
+++ nodejs/express-project/routes/messages.router.js
@@ -1,18 +1,8 @@
 const express = require('express');
+const { getMessages } = require('../controllers/messages.controller');
 
 const messagesRouter = express.Router();
 
-messagesRouter.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <body>
-        <ul>
-          <li>Hello Isacc!</li>
-          <li>What are your thoughts on astronomy?</li>
-        </ul>
-      </body>
-    </html
-  `);
-});
+messagesRouter.get('/', getMessages);
 
 module.exports = messagesRouter;
