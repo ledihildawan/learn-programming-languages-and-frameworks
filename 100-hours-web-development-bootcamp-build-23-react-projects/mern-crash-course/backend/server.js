73c227ebedad72cb7f45fc3718749cdd2adc1621
Ledi Hildawan <lhildawan@gmail.com>
2025-04-24 01:16:19 +0700

    mern crash course deployment

--- 100-hours-web-development-bootcamp-build-23-react-projects/mern-crash-course/backend/server.js
+++ 100-hours-web-development-bootcamp-build-23-react-projects/mern-crash-course/backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -8,7 +13,15 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
 app.listen(5000, () => {
   connectDB();
-  console.log(`Server started at http://locahost:5000`);
+  console.log(`Server started at http://localhost:5000`);
 });
