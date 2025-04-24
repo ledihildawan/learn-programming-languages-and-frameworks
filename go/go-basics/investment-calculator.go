29d7175b81f96dfc3ea3281456155989885bd1ba
Ledi Hildawan <lhildawan@gmail.com>
2025-04-24 15:46:46 +0700

    Understading Functions

--- go/go-basics/investment-calculator.go
+++ go/go-basics/investment-calculator.go
@@ -5,22 +5,29 @@ import (
 	"math"
 )
 
+func outputText(text string) {
+	fmt.Print(text)
+}
+
 func main() {
 	const inflationRate = 2.5
 
 	var investmentAmount, years, expectedReturnRate float64
 
-	fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("Future Value: %v\nFuture Value (adjusted for Inflation): %v", futureValue, futureRealValue)
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for Inflation): %.1f\n", futureRealValue)
+
+	fmt.Print(formattedFV, formattedRFV)
 }
