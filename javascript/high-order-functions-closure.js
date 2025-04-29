6bc66fbc7c50bede859e301acfe5bec96cdd8fdf
Ledi Hildawan <lhildawan@gmail.com>
2025-04-29 23:59:21 +0700

    currying

--- javascript/high-order-functions-closure.js
+++ javascript/high-order-functions-closure.js
@@ -18,3 +18,13 @@ const getCounter = closure();
 getCounter();
 getCounter();
 getCounter();
+
+// Currying
+const multiply = (a, b) => a * b;
+const curriedMultiply = (a) => (b) => a * b;
+const curriedMultiplyBy5 = curriedMultiply(5);
+
+curriedMultiply(5, 3);
+curriedMultiplyBy5(10);
+curriedMultiplyBy5(20);
+curriedMultiplyBy5(2);
