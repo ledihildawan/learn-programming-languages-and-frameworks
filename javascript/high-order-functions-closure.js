21a719deb990cd67dc6baed2aa5e551d12c86f43
Ledi Hildawan <lhildawan@gmail.com>
2025-04-30 09:02:00 +0700

    worker thread in action

--- javascript/high-order-functions-closure.js
+++ javascript/high-order-functions-closure.js
@@ -28,3 +28,27 @@ curriedMultiply(5, 3);
 curriedMultiplyBy5(10);
 curriedMultiplyBy5(20);
 curriedMultiplyBy5(2);
+
+// MemoizedAddTo80
+function addTo80n(n) {
+  console.log('long time');
+  return n + 80;
+}
+
+function memoizedAddTo80(n) {
+  let cache = {};
+  return function memoizedAddTo80(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      cache[n] = n + 80;
+
+      return cache[n];
+    }
+  };
+}
+
+const memoized = memoizedAddTo80();
+
+console.log('1', memoized(5));
+console.log('2', memoized(5));
