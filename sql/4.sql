a5457ce9bf717fc25e4befef0d525c276ef2cc2c
Ledi Hildawan <lhildawan@gmail.com>
2025-05-10 15:19:53 +0700

    LIMITING Results with LIMIT

--- sql/4.sql
+++ sql/4.sql
@@ -23,20 +23,52 @@ CREATE TABLE
         title VARCHAR NOT NULL,
         author VARCHAR NOT NULL,
         price DECIMAL(10, 2) NOT NULL,
-        discount INTEGER DEFAULT 0
+        stock INT NOT NULL discount INTEGER DEFAULT 0,
     );
 
 INSERT INTO
-    books (title, author, price, discount)
+    books (title, author, price, stock, discount)
 VALUES
     (
         'The Great Gatsby',
         'F. Scott Fitzgerald',
-        10.99,
+        15.99,
+        5,
         1
     ),
-    ('1984', 'George Orwell', 12.99, 2),
-    ('To Kill a Mockingbird', 'Harper Lee', 8.99, 3);
+    (
+        'To Kill a Mockingbird',
+        'Harper Lee',
+        15.99,
+        8,
+        3
+    ),
+    ('1984', 'George Orwell', 12.99, 3, 2),
+    (
+        'Pride and Prejudice',
+        'Jane Austen',
+        10.99,
+        10,
+        0
+    ),
+    (
+        'The Catcher in the Rye',
+        'J.D. Salinger',
+        14.99,
+        2,
+        1
+    ),
+    ('The Hobbit', 'J.R.R. Tolkien', 20.99, 4, 0),
+    ('Fahrenheit 451', 'Ray Bradbury', 18.99, 6, 2),
+    ('Brave New World', 'Aldous Huxley', 16.99, 7, 1),
+    (
+        'The Picture of Dorian Gray',
+        'Oscar Wilde',
+        11.99,
+        9,
+        0
+    ),
+    ('The Alchemist', 'Paulo Coelho', 13.99, 1, 0);
 
 SELECT
     title,
@@ -81,7 +113,7 @@ SELECT
 FROM
     books;
 
--- Selecting Distinct Values
+-- Selecting DISTINCT Values
 CREATE TABLE
     orders (
         id SERIAL PRIMARY KEY,
@@ -108,3 +140,217 @@ SELECT DISTINCT
     order_date
 FROM
     orders;
+
+-- Filtering with WHERE
+CREATE TABLE
+    customers (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        registred_at DATE NOT NULL,
+        is_active BOOLEAN NOT NULL,
+        country VARCHAR NOT NULL
+    );
+
+INSERT INTO
+    customers (name, registred_at, is_active, country)
+VALUES
+    ('John Doe', '2024-01-01', TRUE, 'USA'),
+    ('Jane Smith', '2024-01-02', FALSE, 'Canada'),
+    ('Alice Johnson', '2024-01-03', TRUE, 'USA'),
+    ('Bob Smith', '2024-01-04', TRUE, 'Canada'),
+    ('Charlie Davis', '2024-01-05', FALSE, 'USA');
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    is_active = TRUE;
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    is_active = TRUE
+    AND country = 'Canada';
+
+CREATE TABLE
+    orders (
+        id SERIAL PRIMARY KEY,
+        customer_id INT NOT NULL,
+        order_date DATE NOT NULL,
+        total_amount DECIMAL(10, 2) NOT NULL
+    );
+
+INSERT INTO
+    orders (customer_id, order_date, totla_amount)
+VALUES
+    (1, '2024-01-01', 100.00),
+    (2, '2024-01-01', 150.00),
+    (3, '2024-01-01', 200.00),
+    (4, '2024-01-01', 250.00),
+    (5, '2024-01-01', 300.00);
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total_amount > 200;
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total_amount BETWEEN 100 AND 200;
+
+SELECT
+    * FORM orders
+WHERE
+    customer_id IN (1, 2, 3);
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name LIKE '%Smith';
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name LIKE 'S%';
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name Like 'Sm_th';
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name Like '%Sm_th';
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    customer_id NOT IN (1, 3);
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    name NOT LIKE '%S';
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total_amount NOT BETWEEN 100 AND 200;
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name NOT LIKE '%i%';
+
+SELECT
+    *
+FROM
+    customers
+WHERE
+    name LIKE 'A%n';
+
+-- Sorting with ORDER BY
+SELECT
+    *
+FROM
+    orders
+ORDER BY
+    total_amount DESC;
+
+SELECT
+    *
+FROM
+    orders
+ORDER BY
+    total_amount ASC;
+
+SELECT
+    *
+FROM
+    books
+ORDER BY
+    price DESC,
+    stock ASC;
+
+SELECT
+    *
+FROM
+    books
+ORDER BY
+    price DESC,
+    stock DESC;
+
+-- LIMITING Results with LIMIT
+SELECT
+    *
+FROM
+    orders
+LIMIT
+    2;
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total > 200
+LIMIT
+    2;
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total > 200
+ORDER BY
+    total DESC
+LIMIT
+    2;
+
+SELECT
+    *
+FROM
+    orders
+WHERE
+    total > 200
+ORDER BY
+    total DESC
+LIMIT
+    2
+OFFSET
+    1;
+
+SELECT
+    *
+FROM
+    order
+ORDER BY
+    total ASC
+LIMIT
+    1;
