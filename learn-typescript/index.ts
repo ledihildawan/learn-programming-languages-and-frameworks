a99844f8f62f2134e59a12e03c18be00fcce94dd
Ledi Hildawan <lhildawan@gmail.com>
2025-05-31 15:00:10 +0700

    Add a Pizza type

--- learn-typescript/index.ts
+++ learn-typescript/index.ts
@@ -1,3 +1,8 @@
+type Pizza = {
+  name: string;
+  price: number;
+};
+
 const menu = [
   { name: 'Margherita', price: 8 },
   { name: 'Pepperoni', price: 10 },
@@ -9,7 +14,13 @@ let cashInRegister = 100;
 let nextOrderId = 1;
 const orderQueue = [];
 
-function addNewPizza(pizzaObj) {
+/**
+ * Challenge: teach TS that the pizzaObj is supposed to be a Pizza type.
+ * Then like before, look through the code to see if there are any new
+ * TS warnings to deal with (😉), and fix those issues
+ */
+
+function addNewPizza(pizzaObj: Pizza) {
   menu.push(pizzaObj);
 }
 
@@ -25,21 +36,15 @@ function placeOrder(pizzaName) {
   return newOrder;
 }
 
-/**
- * Challenge: Teach TS what data type should be used for the
- * orderId in the completeOrder function. Then check for any
- * additional warnings TS comes up with and fix those.
- */
-
 function completeOrder(orderId: number) {
   const order = orderQueue.find((order) => order.id === orderId);
   order.status = 'completed';
   return order;
 }
 
-addNewPizza({ name: 'Chicken Bacon Ranch', cost: 12 });
-addNewPizza({ name: 'BBQ Chicken', cost: 12 });
-addNewPizza({ name: 'Spicy Sausage', cost: 11 });
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
 
 placeOrder('Chicken Bacon Ranch');
 completeOrder(1);
