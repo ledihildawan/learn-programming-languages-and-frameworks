ff5061b7f9257b78714d1bb57865f10bb7e86622
Ledi Hildawan <lhildawan@gmail.com>
2025-05-31 23:26:43 +0700

    Add return type to getPizzaDetail

--- learn-typescript/index.ts
+++ learn-typescript/index.ts
@@ -1,54 +1,79 @@
 type Pizza = {
+  id: number;
   name: string;
   price: number;
 };
 
-const menu = [
-  { name: 'Margherita', price: 8 },
-  { name: 'Pepperoni', price: 10 },
-  { name: 'Hawaiian', price: 10 },
-  { name: 'Veggie', price: 9 },
+type Order = {
+  id: number;
+  pizza: Pizza;
+  status: 'ordered' | 'completed';
+};
+
+const menu: Pizza[] = [
+  { id: 1, name: 'Margherita', price: 8 },
+  { id: 2, name: 'Pepperoni', price: 10 },
+  { id: 3, name: 'Hawaiian', price: 10 },
+  { id: 4, name: 'Veggie', price: 9 },
 ];
 
 let cashInRegister = 100;
 let nextOrderId = 1;
-const orderQueue = [];
-
-/**
- * Challenge: teach TS that the pizzaObj is supposed to be a Pizza type.
- * Then like before, look through the code to see if there are any new
- * TS warnings to deal with (😉), and fix those issues
- */
+const orderQueue: Order[] = [];
 
 function addNewPizza(pizzaObj: Pizza) {
   menu.push(pizzaObj);
 }
 
-function placeOrder(pizzaName) {
+function placeOrder(pizzaName: string) {
   const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
   if (!selectedPizza) {
     console.error(`${pizzaName} does not exist in the menu`);
     return;
   }
   cashInRegister += selectedPizza.price;
-  const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
+  const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: 'ordered' };
   orderQueue.push(newOrder);
   return newOrder;
 }
 
 function completeOrder(orderId: number) {
   const order = orderQueue.find((order) => order.id === orderId);
+  if (!order) {
+    console.error(`${orderId} was not found in the orderQueue`);
+    return;
+  }
   order.status = 'completed';
   return order;
 }
 
-addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
-addNewPizza({ name: 'BBQ Chicken', price: 12 });
-addNewPizza({ name: 'Spicy Sausage', price: 11 });
+/**
+ * Challenge (part 2): explicitly type the return value of this function
+ * to tell TypeScript it could either be a Pizza object or undefined
+ * as the return value.
+ */
 
-placeOrder('Chicken Bacon Ranch');
-completeOrder(1);
+export function getPizzaDetail(identifier: string | number): Pizza | undefined {
+  if (typeof identifier === 'string') {
+    return menu.find((pizza) => pizza.name.toLowerCase() === identifier.toLowerCase());
+  } else if (typeof identifier === 'number') {
+    return menu.find((pizza) => pizza.id === identifier);
+  } else {
+    throw new TypeError('Parameter `identifier` must be either a string or a number');
+  }
+}
 
-console.log('Menu:', menu);
-console.log('Cash in register:', cashInRegister);
-console.log('Order queue:', orderQueue);
+// addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
+// addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
+// addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
+
+// placeOrder("Chicken Bacon Ranch")
+// placeOrder("Pepperoni")
+// completeOrder(1)
+// placeOrder("Anchovy")
+// placeOrder("Veggie")
+// completeOrder(2)
+
+// console.log("Menu:", menu)
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
