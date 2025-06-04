cf442016135cf0d0d7b64079db7258ebdd7af336
Ledi Hildawan <lhildawan@gmail.com>
2025-06-04 11:55:40 +0700

    useless chatbot

---
+++ useless-chatbot/index.js
@@ -0,0 +1,7 @@
+const talkBtn = document.getElementById('talk-btn');
+const chatInput = document.getElementById('chat-input');
+
+talkBtn.addEventListener('click', function () {
+  console.log(chatInput.value);
+  chatInput.value = '';
+});
