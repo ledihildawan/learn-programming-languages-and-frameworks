821afa43e84f29b2a2d052feafc69b5bc026faa4
Ledi Hildawan <lhildawan@gmail.com>
2025-06-09 22:34:29 +0700

    render tweet

---
+++ E - explain the concept with gaps left f.md
@@ -0,0 +1,17 @@
+E - explain the concept with gaps left for you to fill
+Q - Quiz yourself with 5 questions
+R - Apply the concept in a real-world
+
+- Explain to AI
+- Create and Solve
+- Performance and code readability
+- Ask about the next advanced
+
+- I don't understand how this [insert concept] works in [insert language]. Can you explain it step by step?
+- I don't understand how the if statement works in JavaScript. Can you explain it step by step?
+
+- In [insert language], what does [insert concept] do exactly? I saw it in the video but need more clarification.
+- In HTML, what does the div do exactly? I saw it in the video but need more clarification.
+
+- I've written this code following the tutorial, but I'm not sure how [insert language] works. Can you provide more details?
+- I've written this JavaScript code, but I'm not sure how event listeners work. Can you provide more details?
--- twitter-clone/index.js
+++ twitter-clone/index.js
@@ -6,14 +6,114 @@ tweetBtn.addEventListener('click', function () {
   console.log(tweetInput.value);
 });
 
-function getFeedHtml() {
-  let feedHtml = ``;
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  }
+});
+
+function handleLikeClick(tweetId) {
+  console.log(tweetId);
   /*
 Challenge:
-1. Add data attributes to each of the  <i> tags. You can call
-   them 'reply', 'like', and 'retweet’.
-2. Each data attribute should hold the tweet's uuid.
+1. Iterate over tweetsData and use the uuid 
+   saved in tweetId to identify the liked
+   tweet's object. Save that object to a 
+   new const called 'targetTweetObj'.
+⚠️ targetTweetObj should hold an object, NOT
+   an array.
+2. Increment targetTweetObj's 'likes' count 
+   by 1.
+3. Log out targetTweetObj.
 */
+  const targetTweetObj = tweetsData.find((tweet) => tweet.uuid === tweetId);
+
+  targetTweetObj.likes += 1;
+
+  console.log(targetTweetObj);
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`;
+  });
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById('feed').innerHTML = getFeedHtml();
+}
+
+render();
+
+const tweetInput = document.getElementById('tweet-input');
+const tweetBtn = document.getElementById('tweet-btn');
+
+tweetBtn.addEventListener('click', function () {
+  console.log(tweetInput.value);
+});
+
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  }
+});
+
+function handleLikeClick(tweetId) {
+  console.log(tweetId);
+  /*
+Challenge:
+1. Iterate over tweetsData and use the uuid 
+   saved in tweetId to identify the liked
+   tweet's object. Save that object to a 
+   new const called 'targetTweetObj'.
+⚠️ targetTweetObj should hold an object, NOT
+   an array.
+2. Increment targetTweetObj's 'likes' count 
+   by 1.
+3. Log out targetTweetObj.
+*/
+  const targetTweetObj = tweetsData.find((tweet) => tweet.uuid === tweetId);
+
+  targetTweetObj.likes += 1;
+
+  console.log(targetTweetObj);
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
 
   tweetsData.forEach(function (tweet) {
     feedHtml += `
