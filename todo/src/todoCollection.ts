74e38f56bc7711f5f8043970a9ecdc27daf454c1
Ledi Hildawan <lhildawan@gmail.com>
2025-07-04 00:20:10 +0700

    Providing access to items in the todoCollection.ts file in the src folder

--- todo/src/todoCollection.ts
+++ todo/src/todoCollection.ts
@@ -1,22 +1,35 @@
 import { TodoItem } from './todoItem';
+
 export class TodoCollection {
-  private nextId: number = 1;
-  private itemMap = new Map<number, TodoItem>();
+  readonly #itemMap = new Map<number, TodoItem>();
+
+  #nextId: number = 1;
+
   constructor(public userName: string, todoItems: TodoItem[] = []) {
-    todoItems.forEach((item) => this.itemMap.set(item.id, item));
+    todoItems.forEach((item) => this.#itemMap.set(item.id, item));
   }
-  addTodo(task: string): number {
-    while (this.getTodoById(this.nextId)) {
-      this.nextId++;
+
+  addTodo(task: string) {
+    while (this.getTodoById(this.#nextId)) {
+      this.#nextId++;
     }
-    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
-    return this.nextId;
+
+    this.#itemMap.set(this.#nextId, new TodoItem(this.#nextId, task));
+
+    return this.#nextId;
   }
-  getTodoById(id: number): TodoItem {
-    return this.itemMap.get(id);
+
+  getTodoById(id: number) {
+    return this.#itemMap.get(id);
   }
+
+  getTodoItems(includeComplete: boolean) {
+    return Array.from(this.#itemMap.values()).filter((item) => includeComplete || !item.complete);
+  }
+
   markComplete(id: number, complete: boolean) {
     const todoItem = this.getTodoById(id);
+
     if (todoItem) {
       todoItem.complete = complete;
     }
