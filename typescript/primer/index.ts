92d3299ba159193bb61c3e849f92487df83388cb
Ledi Hildawan <lhildawan@gmail.com>
2025-07-11 23:58:10 +0700

    Adding statements in the index.ts file in the primer folder

--- typescript/primer/index.ts
+++ typescript/primer/index.ts
@@ -1,2 +1,11 @@
 let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
+let bootsPrice = '100';
+console.log(`Boots price: ${bootsPrice}`);
+if (hatPrice == bootsPrice) {
+  console.log('Prices are the same');
+} else {
+  console.log('Prices are different');
+}
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price: ${totalPrice}`);
