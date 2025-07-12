3009c959eea05de3e43ea9e81164277944e30741
Ledi Hildawan <lhildawan@gmail.com>
2025-07-12 19:00:53 +0700

    Using the spread operator in the index.js file in the primer folder

--- typescript/primer/dist/index.js
+++ typescript/primer/dist/index.js
@@ -3,16 +3,31 @@ let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = '100';
 console.log(`Boots price: ${bootsPrice}`);
+function sumPrices(...numbers) {
+    return numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
+}
+let taxRate; // no tax rate has been defined
+console.log(`Tax rate: ${taxRate ?? 10}%`);
+taxRate = 0; // zero-rated for tax
+console.log(`Tax rate: ${taxRate ?? 10}%`);
 if (hatPrice === bootsPrice) {
     console.log('Prices are the same');
 }
 else {
     console.log('Prices are different');
 }
-let totalPrice = Number(hatPrice) + Number(bootsPrice);
+let totalPrice = sumPrices(hatPrice, bootsPrice);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
 console.log(`Total Price: ${totalPrice}`);
 let myVariable = 'Adam';
 console.log(`Type: ${typeof myVariable}`);
 myVariable = 100;
 console.log(`Type: ${typeof myVariable}`);
+let firstCity;
+let secondCity = firstCity || 'London';
+console.log(`City: ${secondCity}`);
 export {};
--- typescript/primer/src/index.ts
+++ typescript/primer/src/index.ts
@@ -1,27 +1,11 @@
-// Number.isNaN(Number(val)) ? 0 : Number(val);
-
-let hatPrice = 100;
-console.log(`Hat price: ${hatPrice}`);
-let bootsPrice = '100';
-console.log(`Boots price: ${bootsPrice}`);
-
-let taxRate; // no tax rate has been defined
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-taxRate = 0; // zero-rated for tax
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-
-if (hatPrice === bootsPrice) {
-  console.log('Prices are the same');
-} else {
-  console.log('Prices are different');
-}
-let totalPrice = Number(hatPrice) + Number(bootsPrice);
-console.log(`Total Price: ${totalPrice}`);
-let myVariable = 'Adam';
-console.log(`Type: ${typeof myVariable}`);
-myVariable = 100;
-console.log(`Type: ${typeof myVariable}`);
-
-let firstCity;
-let secondCity = firstCity || 'London';
-console.log(`City: ${secondCity}`);
+let names = ['Hat', 'Boots', 'Gloves'];
+let prices = [];
+prices.push(100);
+prices.push('100');
+prices.push(50.25);
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
+let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
+let totalPrice = sumPrices(...prices);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+let combinedArray = [...names, ...prices];
+combinedArray.forEach((element) => console.log(`Combined Array Element: ${element}`));
