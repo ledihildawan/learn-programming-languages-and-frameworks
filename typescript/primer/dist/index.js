ea77e3084e6a00fc28b31c5e1f5106dd13f9a1e6
Ledi Hildawan <lhildawan@gmail.com>
2025-07-12 13:03:06 +0700

    Using the nullish operator in the index.js file in the primer folder

---
+++ typescript/primer/dist/index.js
@@ -0,0 +1,18 @@
+// Number.isNaN(Number(val)) ? 0 : Number(val);
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+let bootsPrice = '100';
+console.log(`Boots price: ${bootsPrice}`);
+if (hatPrice === bootsPrice) {
+    console.log('Prices are the same');
+}
+else {
+    console.log('Prices are different');
+}
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
+console.log(`Total Price: ${totalPrice}`);
+let myVariable = 'Adam';
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
+export {};
---
+++ typescript/primer/src/index.ts
@@ -0,0 +1,27 @@
+// Number.isNaN(Number(val)) ? 0 : Number(val);
+
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
+let bootsPrice = '100';
+console.log(`Boots price: ${bootsPrice}`);
+
+let taxRate; // no tax rate has been defined
+console.log(`Tax rate: ${taxRate ?? 10}%`);
+taxRate = 0; // zero-rated for tax
+console.log(`Tax rate: ${taxRate ?? 10}%`);
+
+if (hatPrice === bootsPrice) {
+  console.log('Prices are the same');
+} else {
+  console.log('Prices are different');
+}
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
+console.log(`Total Price: ${totalPrice}`);
+let myVariable = 'Adam';
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || 'London';
+console.log(`City: ${secondCity}`);
