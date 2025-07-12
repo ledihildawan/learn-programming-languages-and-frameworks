3c0dc9a852010602e6d6a4a67c09817bf8fd4083
Ledi Hildawan <lhildawan@gmail.com>
2025-07-12 19:50:11 +0700

    Assigning remaining elements in the index.js file in the primer folder

--- typescript/primer/src/index.ts
+++ typescript/primer/src/index.ts
@@ -1,11 +1,25 @@
 let names = ['Hat', 'Boots', 'Gloves'];
 let prices = [];
+
 prices.push(100);
 prices.push('100');
 prices.push(50.25);
+
 console.log(`First Item: ${names[0]}: ${prices[0]}`);
+
 let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
 let totalPrice = sumPrices(...prices);
+
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+
 let combinedArray = [...names, ...prices];
+
 combinedArray.forEach((element) => console.log(`Combined Array Element: ${element}`));
+
+let [one, two] = names;
+console.log(`One: ${one}, Two: ${two}`);
+
+let [, , three] = names;
+console.log(`Three: ${three}`);
+let [, ...highest] = prices.sort((a, b) => a - b);
+highest.forEach((price) => console.log(`High price: ${price}`));
