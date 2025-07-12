a663adeac27fc42a19bce43a61d351abed4f3b8a
Ledi Hildawan <lhildawan@gmail.com>
2025-07-12 20:32:42 +0700

    Using the fat arrow syntax in the index.js file in the primer folder

--- typescript/primer/src/index.ts
+++ typescript/primer/src/index.ts
@@ -1,25 +1,26 @@
-let names = ['Hat', 'Boots', 'Gloves'];
-let prices = [];
-
-prices.push(100);
-prices.push('100');
-prices.push(50.25);
-
-console.log(`First Item: ${names[0]}: ${prices[0]}`);
-
-let sumPrices = (...numbers) => numbers.reduce((total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
-let totalPrice = sumPrices(...prices);
-
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let combinedArray = [...names, ...prices];
-
-combinedArray.forEach((element) => console.log(`Combined Array Element: ${element}`));
-
-let [one, two] = names;
-console.log(`One: ${one}, Two: ${two}`);
-
-let [, , three] = names;
-console.log(`Three: ${three}`);
-let [, ...highest] = prices.sort((a, b) => a - b);
-highest.forEach((price) => console.log(`High price: ${price}`));
+let hat = {
+  name: 'Hat',
+  _price: 100,
+  priceIncTax: 100 * 1.2,
+  set price(newPrice) {
+    this._price = newPrice;
+    this.priceIncTax = this._price * 1.2;
+  },
+  get price() {
+    return this._price;
+  },
+  writeDetails: () => console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`),
+};
+let boots = {
+  name: 'Boots',
+  price: '100',
+  get priceIncTax() {
+    return Number(this.price) * 1.2;
+  },
+};
+hat.writeDetails();
+hat.price = 120;
+hat.writeDetails();
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+boots.price = '120';
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
