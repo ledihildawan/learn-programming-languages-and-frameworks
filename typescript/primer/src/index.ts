2bb4fddbaa0aaca3e2f01006d1c3bb4af71f8ec3
Ledi Hildawan <lhildawan@gmail.com>
2025-07-13 16:25:17 +0700

    Chaining constructor functions in the index.js file in the primer folder

--- typescript/primer/src/index.ts
+++ typescript/primer/src/index.ts
@@ -1,30 +1,30 @@
-let hat = {
-  name: 'Hat',
-  _price: 100,
-  priceIncTax: 100 * 1.2,
-  set price(newPrice) {
-    this._price = newPrice;
-    this.priceIncTax = this._price * 1.2;
-  },
-  get price() {
-    return this._price;
-  },
-  writeDetails() {
-    console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`);
-  },
-};
-let boots = {
-  name: 'Boots',
-  price: '100',
-  get priceIncTax() {
-    return Number(this.price) * 1.2;
-  },
+let Product = function (name, price) {
+  this.name = name;
+  this.price = price;
 };
 
-hat.writeDetails = hat.writeDetails.bind(hat);
-hat.writeDetails();
-hat.price = 120;
-hat.writeDetails();
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
-boots.price = '120';
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+Product.prototype.toString = function () {
+  return `toString: Name: ${this.name}, Price: ${this.price}`;
+};
+
+let TaxedProduct = function (name, price, taxRate) {
+  Product.call(this, name, price);
+  this.taxRate = taxRate;
+};
+
+Object.setPrototypeOf(TaxedProduct.prototype, Product.prototype);
+
+TaxedProduct.prototype.getPriceIncTax = function () {
+  return Number(this.price) * this.taxRate;
+};
+
+TaxedProduct.prototype.toTaxString = function () {
+  let chainResult = Product.prototype.toString.call(this);
+  return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
+};
+
+let hat = new TaxedProduct('Hat', 100, 1.2);
+let boots = new Product('Boots', 100);
+
+console.log(hat.toTaxString());
+console.log(boots.toString());
