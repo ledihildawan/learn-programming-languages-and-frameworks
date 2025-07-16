e6c35ada72c2076c1c96256c19dbea9b74c8c7b8
Ledi Hildawan <lhildawan@gmail.com>
2025-07-16 22:14:40 +0700

    Listing 7.9 Defining a function in the index.ts file in the src folder

--- typescript/types/src/index.ts
+++ typescript/types/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello, TypeScript');
+function calculateTax(amount) {
+  return amount * 1.2;
+}
+console.log(`${12} = ${calculateTax(12)}`);
+console.log(`${'Hello'} = ${calculateTax('Hello')}`);
+console.log(`${true} = ${calculateTax(true)}`);
