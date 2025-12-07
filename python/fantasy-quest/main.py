59aa909d89039e321bcbf977b8a9f527daf4d0be
Ledi Hildawan <lhildawan@gmail.com>
2025-12-07 19:04:11 +0700

    learn async

--- python/fantasy-quest/main.py
+++ python/fantasy-quest/main.py
@@ -39,3 +39,33 @@ print(
 )
 print(f"magic_resistance: {type(magic_resistance).__name__}")
 print(f"account_active: {type(account_active).__name__}")
+
+
+def triple_attack(damage_one, damage_two, damage_three):
+    return damage_one + damage_two + damage_three
+
+
+# Don't touch below this line
+
+# This is the first triple attack
+attack_one = 2
+attack_two = 4
+attack_three = 3
+first_triple_attack_damage = triple_attack(attack_one, attack_two, attack_three)
+
+print("Getting damage for", attack_one, attack_two, "and", attack_three, "...")
+print(first_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
+
+# This is the second triple attack
+attack_four = -1
+attack_five = 10
+attack_six = 5
+second_triple_attack_damage = triple_attack(attack_four, attack_five, attack_six)
+
+print("Getting damage for", attack_four, attack_five, "and", attack_six, "...")
+print(second_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
+
+
+## function to add your current question data to a cookie
