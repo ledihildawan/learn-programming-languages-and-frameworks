33886055a87274d903d9153b85b3cfa923bd6eef
Ledi Hildawan <lhildawan@gmail.com>
2025-12-11 13:22:37 +0700

    Make a content collection

---
+++ astro/tutorial/src/content.config.ts
@@ -0,0 +1,23 @@
+// Import the glob loader
+import { glob } from 'astro/loaders';
+// Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+// Import Zod
+import { z } from 'astro/zod';
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/blog' }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
