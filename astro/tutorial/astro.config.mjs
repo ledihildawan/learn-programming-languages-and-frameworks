a9d14f09448cf89a2ed24ec55f0e58004336c18b
Ledi Hildawan <lhildawan@gmail.com>
2025-12-11 12:34:59 +0700

    Finised astro tutorial

--- astro/tutorial/astro.config.mjs
+++ astro/tutorial/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   site: 'http://localhost:4321',
+  integrations: [preact()],
 });
